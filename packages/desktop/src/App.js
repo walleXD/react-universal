95ec06ab26f790753661e31c288f554f039e6b26
Walee Ahmed <walee.tausif@gmail.com>
2017-11-16 21:46:05 -0500

    added new electron app using electron-compile

--- packages/desktop/src/App.js
+++ packages/desktop/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import { Test } from '@universal-react/components'
 
 const App = () =>
   <View>
+    <Text>dumbo</Text>
     <Test />
   </View>
 
